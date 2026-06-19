f436d55b908a3dfe0b087a1941765c5e9bd647f1
hey-jaswanth <heyjaswanth.dev@gmail.com>
2026-06-19 23:26:46 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        if(prices.length <= 1) return 0;
+
+        int minSoFar = prices[0];
+        int profit = 0;
+
+        for(int i = 1; i < prices.length; i++) {
+
+            profit = Math.max(profit, prices[i] - minSoFar);
+            minSoFar = Math.min(minSoFar, prices[i]);
+
+        }
+
+        return profit;
+
+    }
+}
