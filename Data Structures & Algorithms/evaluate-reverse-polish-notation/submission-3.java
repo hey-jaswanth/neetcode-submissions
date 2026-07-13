825c7d7af6914657e1c2720c8c8f5737cca3e537
hey-jaswanth <heyjaswanth.dev@gmail.com>
2026-07-13 08:56:19 +0530

    Add: evaluate-reverse-polish-notation - submission-3

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-3.java
@@ -0,0 +1,39 @@
+
+
+class Solution {
+
+    public int evalRPN(String[] tokens) {
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (String token : tokens) {
+            if (isOperator(token)) {
+                int right = stack.pop();
+                int left = stack.pop();
+                stack.push(applyOperator(left, right, token));
+            } else {
+                stack.push(Integer.parseInt(token));
+            }
+        }
+
+        return stack.pop();
+    }
+
+    private boolean isOperator(String token) {
+        return token.length() == 1 && "+-*/".contains(token);
+    }
+
+    private int applyOperator(int left, int right, String operator) {
+        switch (operator) {
+            case "+":
+                return left + right;
+            case "-":
+                return left - right;
+            case "*":
+                return left * right;
+            case "/":
+                return left / right;
+            default:
+                throw new IllegalArgumentException("Invalid operator: " + operator);
+        }
+    }
+}
